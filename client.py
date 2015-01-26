f6d7b679eb8b23ce459b1a4cac1d4a2be490e728
Steve Pomeroy <stevep@thelevelup.com>
2015-01-26 13:25:36 -0500

    Adds reconnection

--- client.py
+++ client.py
@@ -14,11 +14,15 @@ import subprocess
 import sys
 from datetime import timedelta
 from tornado.ioloop import IOLoop
+from tornado.websocket import WebSocketClosedError
+from socket import gaierror
 
 class Bell():
     PING_TIMEOUT = 15
+    RECONNECT_TIMEOUT = 1
     conn = None
     keepalive = None
+    reconnection = None
     host = None
 
     def __init__(self, host):
@@ -29,21 +33,31 @@ class Bell():
     def connect(self):
         ws = tornado.websocket.websocket_connect("ws://%s/dingsocket" % self.host)
         ws.add_done_callback(self.conn_callback)
+        self.ws = ws
 
     def dokeepalive(self):
         stream = self.conn.protocol.stream
+        stream.set_close_callback(self.reconnect_delayed)
         if not stream.closed():
            self.keepalive = stream.io_loop.add_timeout(
                    timedelta(seconds=self.PING_TIMEOUT), self.dokeepalive)
-           self.conn.protocol.write_ping("")
+           try:
+               self.conn.protocol.write_ping("")
+           except WebSocketClosedError:
+               self.reconnect_delayed()
         else:
             self.keepalive = None # should never happen
 
     def conn_callback(self, conn):
-        self.conn = conn.result()
-        self.conn.on_message = self.on_message
-        self.keepalive = IOLoop.instance().add_timeout(
-                timedelta(seconds=self.PING_TIMEOUT), self.dokeepalive)
+        try:
+            self.conn = conn.result()
+            self.conn.on_message = self.on_message
+            self.keepalive = IOLoop.instance().add_timeout(
+                    timedelta(seconds=self.PING_TIMEOUT), self.dokeepalive)
+        except WebSocketClosedError:
+           self.reconnect_delayed()
+        except gaierror:
+           self.reconnect_delayed()
 
     def on_message(self, message):
         if message is not None:
@@ -53,9 +67,18 @@ class Bell():
             subprocess.Popen(["paplay", "bell.ogg"],
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
-            self.reconnect()
+            self.reconnect_delayed()
+
+    def reconnect_delayed(self):
+        if self.reconnect is not None:
+            reconnection = self.reconnection
+            self.reconnection = None
+            IOLoop.instance().remove_timeout(reconnection)
+        self.reconnection = IOLoop.instance().add_timeout(
+                timedelta(seconds=self.RECONNECT_TIMEOUT), self.reconnect)
 
     def reconnect(self):
+        print("Attempting to reconnect...")
         if self.keepalive is not None:
             keepalive = self.keepalive
             self.keepalive = None
