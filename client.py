bb63d015e49e60531cfad9e51fa64e3730067168
Steve Pomeroy <stevep@thelevelup.com>
2016-02-24 10:51:32 -0500

    Move to config file; add special sounds

--- client.py
+++ client.py
@@ -9,11 +9,26 @@ import tornado.websocket
 
 import subprocess
 import sys
-from datetime import timedelta
+from datetime import timedelta, datetime
 from tornado.ioloop import IOLoop
 from tornado.websocket import WebSocketClosedError
 from socket import gaierror
 import socket
+import json
+
+class Play():
+    def play(self, sound_file):
+        pass
+
+class AudioFilePlay(Play):
+    def play(self, sound_file):
+        subprocess.Popen(["afplay", sound_file],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+class PulseAudioPlay(Play):
+    def play(self, sound_file):
+        subprocess.Popen(["paplay", sound_file],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 class Bell():
     PING_TIMEOUT = 15
@@ -22,10 +37,16 @@ class Bell():
     keepalive = None
     reconnection = None
     host = None
+    special_alerts = None
+    playback = None
+    bell = None
 
-    def __init__(self, host):
+    def __init__(self, host, playback, bell, special_alerts=None):
         self.host = host
         self.connect()
+        self.special_alerts = special_alerts
+        self.playback = playback
+        self.bell = bell
 
     def connect(self):
         ws = tornado.websocket.websocket_connect("ws://%s/dingsocket" % self.host)
@@ -51,6 +72,7 @@ class Bell():
             self.conn.on_message = self.on_message
             self.keepalive = IOLoop.instance().add_timeout(
                     timedelta(seconds=self.PING_TIMEOUT), self.dokeepalive)
+            print("Connected.")
         except WebSocketClosedError:
            self.reconnect_delayed()
         except gaierror:
@@ -58,9 +80,11 @@ class Bell():
 
     def on_message(self, message):
         if message is not None:
-            print("Ding! %s" % message)
-            subprocess.Popen(["paplay", "bell.ogg"],
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print("%s: Ding! %s" % (datetime.now(), message))
+            sound_file = self.bell
+            if message in self.special_alerts:
+                sound_file = self.special_alerts[message]
+            self.playback.play(sound_file)
         else:
             self.reconnect_delayed()
 
@@ -86,12 +110,20 @@ def main():
     try:
         io_loop = IOLoop.instance()
         signal.signal(signal.SIGTERM, io_loop.stop)
-        bell = Bell(sys.argv[1])
+        if len(sys.argv) < 2:
+            print("%s config_file.json" % sys.argv[0])
+            sys.exit(1)
+
+        config = {}
+        with open(sys.argv[1]) as config_file:
+            config = json.load(config_file)
+
+        bell = Bell(config['server'], PulseAudioPlay(), config['bell'], config['special_alerts'])
         io_loop.start()
     except KeyboardInterrupt:
         io_loop.stop()
-    except OSError:
-        print("OSError")
+    except OSError as e:
+        print("OSError: %s" % e)
         if bell:
             bell.reconnect_delayed()
     except socket.error:
